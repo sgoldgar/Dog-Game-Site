0be490e39847f7b4cc56538c42796615ef6d547e
Sarah Goldgar <sgoldgar@gmail.com>
2017-06-06 13:52:35 -0500

    edited about

--- src/About.js
+++ src/About.js
@@ -6,8 +6,8 @@ class About extends Component {
     return (
       <section id="about" className="about">
         <h2 className="section-title">ABOUT</h2>
-        <p>This game is made by Sarah Goldgar. She is a front-end web developer and pixel art enthusiast. Having a lot of dog friends and one time, a dog birthday party, she got the idea for a game based in her hometown to teach younger children about problem solving and iconic locations in Austin. All art is made by her in Photoshop and the game is coded in Unity using Javascript. This site was made using ReactJS.</p>
-        <p>You can follow game updates <span><a href="https://ginjamgames.tumblr.com/" target="_blank">here</a></span>.</p>
+        <p>This game is made by Sarah Goldgar. She is a front-end web developer and pixel art enthusiast. Having a lot of dog friends and one time, a dog birthday party, she got the idea for a game based in her hometown to teach younger children about problem solving and iconic locations in Austin. All art is made by her in Photoshop and the game is coded in React. Hopefully the game will later be migrated to Unity for better cross-platform function. This site was made using ReactJS.</p>
+        <p>You can follow game updates <span><a href="https:ginjamgames.tumblr.com/" target="_blank">here</a></span>.</p>
     </section>
     );
   }
